2fb72c097cd336eeea149638eb2685e7980ec4bf
Gregor <gluastoned@gmail.com>
2019-07-05 12:09:15 +0200

    :sparkles: added -y to certbot

--- scripts/certbot.sh
+++ scripts/certbot.sh
@@ -4,8 +4,8 @@
 sudo apt install software-properties-common -y
 
 ## certbot repository
-sudo add-apt-repository universe
-sudo add-apt-repository ppa:certbot/certbot
+sudo add-apt-repository universe -y
+sudo add-apt-repository ppa:certbot/certbot -y
 
 ## install certbot
 sudo apt install certbot python-certbot-nginx -y
