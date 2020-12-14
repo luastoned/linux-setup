036e8047dd791bda239a570f7953bf935e35c93b
Gregor <gluastoned@gmail.com>
2020-12-14 11:28:39 +0100

    :wrench: update node / docker

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -3,6 +3,9 @@
 ## dependencies
 sudo apt install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 
+## remove old versions
+sudo apt remove docker docker-engine docker.io containerd runc
+
 ## docker repository
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
--- scripts/node.sh
+++ scripts/node.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## dependencies
-sudo apt install software-properties-common -y
-
-## nvm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+sudo apt install software-properties-common gpg-agent -y
 
 ## yarn repository
 curl -fsSL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
 sudo add-apt-repository "deb https://dl.yarnpkg.com/debian/ stable main"
+
+## nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 ## install yarn
 sudo apt install --no-install-recommends yarn -y
