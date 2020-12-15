c7c898f98753b635441fb41002e4e8996464b5e1
Gregor <gluastoned@gmail.com>
2020-12-15 17:45:04 +0100

    :wrench: add docker compose

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -11,4 +11,4 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 ## install docker
-sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install docker-ce docker-ce-cli containerd.io docker-compose -y
