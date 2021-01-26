e52ac801a85d52ad321377b0ab9aedeac62610a8
Gregor <gluastoned@gmail.com>
2021-01-26 16:17:22 +0100

    :watch: added inotify consumers reference

--- scripts/inotify-watchers.sh
+++ scripts/inotify-watchers.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 ## https://github.com/guard/listen/wiki/Increasing-the-amount-of-inotify-watchers
+## https://github.com/fatso83/dotfiles/blob/master/utils/scripts/inotify-consumers
+
 echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p
